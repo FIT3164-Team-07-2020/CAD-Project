beade8ffb123da39121021f9b6ebd48df5844fd4
Yiqiu Zhu <yzhu0025@student.monash.edu>
2020-08-07 14:09:29 +1000

    Yiqiu: deal with special values

--- Preprocessing_Yiqiu.R
+++ Preprocessing_Yiqiu.R
@@ -1,3 +1,8 @@
 # Package "readxl" is used for reading "xlsx / xls" files.
 library(readxl)
 ZAS_Original <- read_excel("Z_Alizadeh_Sani_Dataset.xlsx")
+
+# Check whether there is any na or null value in the dataset.
+# The results indicate there is no special value in the dataset.
+sum(is.na(ZAS_Original))
+sum(is.null(ZAS_Original))
