543477bfd583811c69002f702f128e14a1fb5aba
AlexYannn <69294643+AlexYannn@users.noreply.github.com>
2020-08-12 19:45:34 +0800

    Add files via upload

---
+++ 3164Preprocessing_XinhaoYan.R
@@ -0,0 +1,21 @@
+setwd("/Users/xinhaoyan/desktop")
+rm(list = ls())
+
+#read the dataset to be data frame
+ZAS.dataset = read.csv("Z-Alizadeh sani dataset.csv")
+
+#find the description
+str(ZAS.dataset)
+
+#set seed to make all teammates have the same random dataset
+set.seed(07070707)
+
+#get 70% of the dataset to be training dataset
+training.rows = sample(1:nrow(ZAS.dataset), 0.7*nrow(ZAS.dataset))
+ZAS.train = ZAS.dataset[training.rows,]
+
+#the rest of rows are testing dataset
+ZAS.test = ZAS.dataset[-training.rows,]
+
+
+
