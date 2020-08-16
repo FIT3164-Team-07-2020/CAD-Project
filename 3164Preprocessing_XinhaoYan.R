28b06a67868c02d1fa5821cb517054d71cf03a48
AlexYannn <a731177398@gmail.com>
2020-08-16 12:55:06 +0800

    factor conversion

--- 3164Preprocessing_XinhaoYan.R
+++ 3164Preprocessing_XinhaoYan.R
@@ -10,19 +10,25 @@ str(ZAS.dataset)
 
 #DLP
 ZAS.dataset$DLP = as.numeric(ZAS.dataset$DLP)-1
+ZAS.dataset$DLP = as.factor(ZAS.dataset$DLP)
 #Weak Peripheral Pulse
 ZAS.dataset$Weak.Peripheral.Pulse = as.numeric(ZAS.dataset$Weak.Peripheral.Pulse)-1
+ZAS.dataset$Weak.Peripheral.Pulse = as.factor(ZAS.dataset$Weak.Peripheral.Pulse)
 #Lung rales
 ZAS.dataset$Lung.rales = as.numeric(ZAS.dataset$Lung.rales)-1
+ZAS.dataset$Lung.rales = as.factor(ZAS.dataset$Lung.rales)
 #Systolic Murmur
 ZAS.dataset$Systolic.Murmur = as.numeric(ZAS.dataset$Systolic.Murmur)-1
+ZAS.dataset$Systolic.Murmur = as.factor(ZAS.dataset$Systolic.Murmur)
 #Diastolic Murmur
 ZAS.dataset$Diastolic.Murmur = as.numeric(ZAS.dataset$Diastolic.Murmur)-1
+ZAS.dataset$Diastolic.Murmur = as.factor(ZAS.dataset$Diastolic.Murmur)
 #Dyspnea
 ZAS.dataset$Dyspnea = as.numeric(ZAS.dataset$Dyspnea)-1
+ZAS.dataset$Dyspnea = as.factor(ZAS.dataset$Dyspnea)
 #Atypical
 ZAS.dataset$Atypical = as.numeric(ZAS.dataset$Atypical)-1
-
+ZAS.dataset$Atypical = as.factor(ZAS.dataset$Atypical)
 
 #Function Class
 ZAS.dataset$Function.Class = as.factor(ZAS.dataset$Function.Class)
