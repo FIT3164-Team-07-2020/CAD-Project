e0100c5b58ab3aa9ac94973b1db77c37da10288c
Yiqiu Zhu <yzhu0025@student.monash.edu>
2020-10-08 15:47:14 +1100

    Improved the prototype

--- Shinyapps_IO/server.R
+++ Shinyapps_IO/server.R
@@ -4,9 +4,11 @@ function(input, output) {
   # Text output.
   output$out <- renderText(paste0(
     "The output can change corresponding to the input:\n",
-    "The input of slider input: ", input$slider0, ";\n",
-    "The input of select input: ", input$select0, ";\n",
-    "The input of radio button input: ", input$radio_button0, ";\n",
-    "The input of numeric input: ", input$numeric0, ".\n\n",
-    "Can also implement a button so that the model will run only when users click."))
+    "The age input: ", input$age, " years;\n",
+    "The weight input: ", input$weight, "kg;\n",
+    "The height input: ", input$height, "cm;\n",
+    "The BMI input: ", input$bmi, ";\n",
+    "The Chest Pain input: ", input$typicalChestPain, ";\n",
+    "The Region RWMA input: ", input$RegionRWMA, ".\n\n",
+    "Maybe implement a button so that the model will run only when users click?"))
 }
--- Shinyapps_IO/ui.R
+++ Shinyapps_IO/ui.R
@@ -5,19 +5,42 @@ fluidPage(
   titlePanel("A Simple Prototype"),
   # Ask users for inputs in the side bar.
   sidebarPanel(
-    # Slider input, usually used for select ranged values.
-    sliderInput('slider0', 'Slider Input Example', min=1, max=100,
-                value=50, step=1, round=0),
-    # Select input, usually used for multi-categorical values.
-    selectInput('select0', 'Select Input Example', 
-                c('None', 3164, '07', 'Data Science Project'),
-                selected = 3164),
-    # Radio button input, usually used for binary values.
-    radioButtons('radio_button0', 'Radio Button Input Example',
-                       choices = c('Yes', 'No'), selected = 'Yes'),
-    # Numerical input, usually used for multi-valued numerical values.
-    numericInput('numeric0', 'Numeric Input Example (ranged 0 to 100)',
-                 0, min = 0, max = 100)
+    
+    # Numerical input for age with the min limitation of 0-year-old, initial 0.
+    numericInput('age',
+                 'Please enter your age in integer years: ',
+                 value = 0,
+                 min = 0),
+    
+    # Numerical input for weight with the min limitation of 1 kilogram, initial 1.
+    numericInput('weight',
+                 'Please enter your weight in integer kilograms: ',
+                 value = 1,
+                 min = 1),
+    
+    # Numerical input for height with the min limitation of 1 centimeter, initial 1.
+    numericInput('height',
+                 'Please enter your height in integer centimeters: ',
+                 value = 1,
+                 min = 1),
+    
+    # Numerical input for BMI with the min limitation of 0.001, initial 0.0001.
+    numericInput('bmi',
+                 'Please enter your BMI (Note: BMI can be calculated using the weight and height obtained): ',
+                 value = 0.0001,
+                 min = 0.0001),
+    
+    # Radio button input for typical chest pain with default value of No.
+    radioButtons('typicalChestPain',
+                 'Do you have a typical chest pain?',
+                 choices = c('Yes', 'No'), selected = 'No'),
+    
+    # Select input, usually used for multi-categorical values, default 0.
+    selectInput('RegionRWMA',
+                'Please enter your Region.RWMA value (some descriptive information should appear here)', 
+                c(0, 1, 2, 3, 4),
+                selected = 0),
+
   ),
   # Print the text output (in a gray box) to users in the main area.
   mainPanel(
