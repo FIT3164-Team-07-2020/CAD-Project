3161c6ace029830384579c6db1fa4efa5e5991bb
Denzel Abad <denzelabad98@gmail.com>
2020-10-13 19:03:23 +1100

    Added all variables into web page and converted them into understandable words

--- Shinyapps_IO/ui.R
+++ Shinyapps_IO/ui.R
@@ -2,44 +2,164 @@ library(shiny)
 
 fluidPage(
   # The title of the UI part
-  titlePanel("A Simple Prototype"),
+  titlePanel("Coronary Artery Disease Prediction Model"),
   # Ask users for inputs in the side bar.
   sidebarPanel(
     
+    #Introductory text
+    h5("*Insert individual patient data in the fields below."),
+    
+    h3("Basic Demographic Information"),
+    
+    # Input for sex, Male or Female
+    radioButtons('sex',
+                 'Sex',
+                 choices = c('Male', 'Female')),
+    
     # Numerical input for age with the min limitation of 0-year-old, initial 0.
     numericInput('age',
-                 'Please enter your age in integer years: ',
+                 'Age (years): ',
                  value = 0,
                  min = 0),
     
     # Numerical input for weight with the min limitation of 1 kilogram, initial 1.
     numericInput('weight',
-                 'Please enter your weight in integer kilograms: ',
-                 value = 1,
-                 min = 1),
+                 'Weight (kg): ',
+                 value = 0,
+                 min = 0),
     
-    # Numerical input for height with the min limitation of 1 centimeter, initial 1.
-    numericInput('height',
-                 'Please enter your height in integer centimeters: ',
-                 value = 1,
-                 min = 1),
+    # Numerical input for length with the min limitation of 1 cm, initial 1.
+    numericInput('length',
+                 'Height (cm): ',
+                 value = 0,
+                 min = 0),
     
     # Numerical input for BMI with the min limitation of 0.001, initial 0.0001.
     numericInput('bmi',
-                 'Please enter your BMI (Note: BMI can be calculated using the weight and height obtained): ',
+                 'BMI (Note: BMI can be calculated using the weight and height obtained): ',
                  value = 0.0001,
                  min = 0.0001),
     
-    # Radio button input for typical chest pain with default value of No.
-    radioButtons('typicalChestPain',
-                 'Do you have a typical chest pain?',
+    h3("Symptom Examination"),
+    
+    # Input for BP (blood pressure)
+    numericInput('BP',
+                 'Blood Pressure (mmHg) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for PR (pulse rate)
+    numericInput('PR',
+                 'Pulse Rate (ppm) ',
+                 value = 0,
+                 min = 0),
+    
+    h5("Select the appropriate option below if the patient has the following:"),
+    
+    # Input for HTN (Hypertension
+    radioButtons('HTN',
+                 'Hypertension',
                  choices = c('Yes', 'No'), selected = 'No'),
     
-    # Select input, usually used for multi-categorical values, default 0.
-    selectInput('RegionRWMA',
-                'Please enter your Region.RWMA value (some descriptive information should appear here)', 
+    # Input for chest pain. Corresponds to Atypical and Typical.Chest.Pain variables
+    # Atypical = atypical chest pain, Typical.Chest.Pain = typical chest pain
+    # If None, then return 0 for both variables.
+    radioButtons('ChestPain',
+                 'Chest pain',
+                 choices = c('None','Typical', 'Atypical'), selected = 'None'),
+    
+    h3("EKG and Echocardiogram Results"),
+    
+    # Input for Tinversion
+    radioButtons('Tinversion',
+                 'T Inversion',
+                 choices = c('Yes', 'No'), selected = 'No'),
+
+    # Input for Region.RWMA
+    selectInput('Region.RWMA',
+                'Region with RWMA (Region Wall Motion Abnormality)', 
                 c(0, 1, 2, 3, 4),
                 selected = 0),
+    
+    h3("Blood Analysis"),
+    
+    # Input for FBS (fasting blood sugar)
+    numericInput('FBS',
+                 'Fasting Blood Sugar (mg/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for Cr (creatine)
+    numericInput('Cr',
+                 'Creatine (mg/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for TG (triglyceride)
+    numericInput('TG',
+                 'Triglyceride (mg/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for LDL (low density lipoprotein)
+    numericInput('LDL',
+                 'Low Density Lipoprotein (mg/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for BUN (blood urea nitrogen)
+    numericInput('BUN',
+                 'Blood Urea Nitrogen (mg/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for ESR (erythrocyte sedimentation rate)
+    numericInput('ESR',
+                 'Erythrocyte Sedimentation Rate (mm/h) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for HB (hemoglobin)
+    numericInput('HB',
+                 'Hemoglobin (g/dl) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for K (potassium)
+    numericInput('K',
+                 'Potassium (mEq/lit) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for NA (sodium)
+    numericInput('NA',
+                 'Sodium (mEq/lit) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for WBC (white blood cell)
+    numericInput('WBC',
+                 'White Blood Cell (cells/ml) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for Lymph (lymphocyte)
+    numericInput('Lymph',
+                 'Lymphocyte (%) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for PLT (platelet)
+    numericInput('PLT',
+                 'Platelet (1000/ml) ',
+                 value = 0,
+                 min = 0),
+    
+    # Input for EF.TTE (ejection fraction)
+    numericInput('EF.TTE',
+                 'Ejection Fraction (%) ',
+                 value = 0,
+                 min = 0),
 
   ),
   # Print the text output (in a gray box) to users in the main area.
